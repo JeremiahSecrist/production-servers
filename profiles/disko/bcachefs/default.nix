484cb15a61a2b0b3ac1672dd0bb41556d0ba49bd
Jermeiah S <owner@arouzing.xyz>
2024-01-29 18:09:40 -0500

    made disko easy to boot

--- profiles/disko/bcachefs/default.nix
+++ profiles/disko/bcachefs/default.nix
@@ -1,36 +1,70 @@
-{lib, ...}: {
+# Example to create a bios compatible gpt partition
+{ lib, ... }:
+{
   disko.devices = {
     disk = {
-      vdb = {
-        device = "/dev/vda";
+      vda = {
         type = "disk";
+        device = "/dev/vda";
         content = {
           type = "gpt";
           partitions = {
-            ESP = {
-              end = "500M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
             };
             root = {
-              name = "root";
-              end = "-0";
+              size = "100%";
               content = {
-                type = "filesystem";
-                format = "bcachefs";
-                mountpoint = "/";
-                mountOptions = ["compression=zstd"];
-              };
-            };
-            swap = {
-              size = "8G";
-              content = {
-                type = "swap";
-                resumeDevice = true; # resume from hiberation from this device
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "/rootfs" = {
+                    mountpoint = "/";
+                  };
+                  # Subvolume name is the same as the mountpoint
+                  "/home" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
+                  };
+                  "/var" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/var";
+                  };
+                  # Parent is not mounted so the mountpoint must be set
+                  "/nix" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/nix";
+                  };
+                  # Subvolume for the swapfile
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap = {
+                      swapfile.size = "8G";
+                    };
+                  };
+                };
+
+                mountpoint = "/partition-root";
+                swap = {
+                  swapfile = {
+                    size = "8G";
+                  };
+                };
               };
             };
           };
@@ -39,3 +73,4 @@
     };
   };
 }
+
