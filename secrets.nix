6db1169e7c0b0684d9fca7e81ef6f1b71facffe0
Jermeiah S <owner@arouzing.xyz>
2024-01-31 14:10:12 -0500

    setup mailserver secrets

--- secrets.nix
+++ secrets.nix
@@ -1,14 +1,15 @@
 let
   machines = {
-    linode = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExj3H8k2iLriwR/Ui9RzIaox+qmy8H1kvKZjNAn9ddZ"; # A normal SSH ed25519 key.
+    mailserver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIh4HizWNDmonN7QRJr7mAcaE6qjCwe4GcvTaZSV7YnR"; # A normal SSH ed25519 key.
   };
-  pth = "secrets";
+  pth = "./secrets";
   users = {
     sky = "age1yubikey1qd07mh0sznx06zvy3qrzfyajeaveerdg4awzp4t4s5dvc7285n4a23hk6j2"; # the public key
   };
 in {
-  "${pth}/nextcloudPassword".publicKeys = [
+
+  "${pth}/mailserver".publicKeys = [
     users.sky
-    machines.linode
+    machines.mailserver
   ];
 }
