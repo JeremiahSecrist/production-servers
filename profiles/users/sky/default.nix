c75ebb6a4192e96940a45912c954b53479e4ad87
Jeremiah S <owner@arouzing.xyz>
2024-02-14 11:37:52 -0500

    update: add new key

--- profiles/users/sky/default.nix
+++ profiles/users/sky/default.nix
@@ -9,6 +9,7 @@
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAGm66rJsr8vjRCYDkH4lEPncPq27o6BHzpmRmkzOiM"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBA9i9HoP7X8Ufzz8rAaP7Nl3UOMZxQHMrsnA5aEQfpTyIQ1qW68jJ4jGK5V6Wv27MMc3czDU1qfFWIbGEWurUHQ="
       ];
       extraGroups = ["wheel" "docker"];
     };
